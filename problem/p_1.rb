80b407f133dd71492928e4145cdb839d6fb6e7ad
Sonny Gonzalez <sonnyg.email@gmail.com>
2014-09-05 15:03:47 -0500

    Add more info the problem output and format better

--- problem/p_1.rb
+++ problem/p_1.rb
@@ -1,16 +1,17 @@
 # problem
-p %q(1. Show the population of Germany)
 puts
-p 'rails way'
-p %q(Country.find_by(name: 'Germany'))
+p         %q(1. Show the population of Germany)
+puts
+p         'rails way'
+p         %q(Country.find_by(name: 'Germany'))
 country = Country.find_by(name: 'Germany')
-puts "population: #{country.population}"
+puts      "population: #{country.population}"
 puts
-p 'sql way'
-p %q(SELECT population FROM country)
-p %q(  WHERE name = 'Germany')
+p         'sql way'
+p         %q(SELECT population FROM country)
+p         %q(  WHERE name = 'Germany')
 puts
 
-# solution
+p 'solution'
 p %q(population = 80716000)
 puts
